d826be5b533e5122c0719e91288a1eb6ebf908bb
vanvurenl1 <vanvurenl1@gmail.com>
2023-08-06 21:37:42 -0400

    add light lazyvim config

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,3 +3,4 @@
 -- Add any additional keymaps here
 vim.keymap.set("n", "<leader>pv", "<cmd>Ex<cr>", {})
 vim.keymap.set({ "i", "n", "v" }, "<C-c>", "<esc>", {})
+vim.keymap.set("n", "<leader>km", "<cmd>Telescope keymaps<cr>", { desc = "Keymaps" })
